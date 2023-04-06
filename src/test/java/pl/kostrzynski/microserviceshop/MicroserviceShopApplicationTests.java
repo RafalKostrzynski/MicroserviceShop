8d2aff142d2184434a5db0fbff214a9a31d5a3dc
RafalKostrzynski <kostrzynskirafal@wp.pl>
2023-04-06 14:14:58 +0200

    Initial commit

---
+++ src/test/java/pl/kostrzynski/microserviceshop/MicroserviceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.microserviceshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
